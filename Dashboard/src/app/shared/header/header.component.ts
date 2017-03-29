a396d8f42866c9edddb7dfc12db0020ec617121a
Pedro Lima <pedrolima.0000@gmail.com>
2017-03-29 10:06:22 +0100

    asdasdas

--- Dashboard/src/app/shared/header/header.component.ts
+++ Dashboard/src/app/shared/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     
     selector: 'shared-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent { }
